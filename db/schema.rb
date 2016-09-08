7218d44c01ab5a911eadedf370f41f0ac2e861e7
david.wei <david.wei@alo7.com>
2016-09-08 13:46:04 +0800

    fix the scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160908030355) do
+ActiveRecord::Schema.define(version: 20160908054322) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string   "title"
+    t.text     "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
